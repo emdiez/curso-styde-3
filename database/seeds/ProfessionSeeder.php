09f9ea6d866646530e8a36ef0a8f72b688872af1
Salomon Machuca Ubuntu 18.04 <salomonmachucaduran@gmail.com>
2020-10-12 14:44:22 -0500

    Leccion 13 Constructor de consultas. 	modified:   database/seeds/ProfessionSeeder.php 	modified:   database/seeds/UserSeeder.php

--- database/seeds/ProfessionSeeder.php
+++ database/seeds/ProfessionSeeder.php
@@ -12,6 +12,10 @@ class ProfessionSeeder extends Seeder
      */
     public function run()
     {
+        // DB::insert('INSERT INTO professions(title) VALUES(?)', ['Desarrollador Front-end']);
+        // DB::insert('INSERT INTO professions(title) VALUES(:title)', ['title' => 'Desarrollador Back-end']);
+        // DB::insert('INSERT INTO professions(title) VALUES(:title)', ['title' => 'Diseñador web']);
+
         DB::table('professions')->insert([
             'title' => 'Desarrollador Back-end'
         ]);
--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -14,11 +14,17 @@ class UserSeeder extends Seeder
     {
         $now = date('Y-m-d H:i:s');
 
+        // $professions = DB::select('SELECT id FROM professions WHERE title=:title LIMIT 0,1', ['title' => 'Desarrollador Back-end']);
+        // $profession = DB::table('professions')->select('id')->where('title', 'Desarrollador Back-end')->take(1)->get();
+        // $profession = DB::table('professions')->select('id')->where('title', 'Desarrollador Back-end')->first();
+        // $profession = DB::table('professions')->select('id')->whereTitle('Desarrollador Back-end')->first();
+        $professionId = DB::table('professions')->where('title', 'Desarrollador Back-end')->value('id');
+
         DB::table('users')->insert([
             'name' => 'Salomon Machuca',
             'email' => 'em_di-es@hotmail.com',
             'password' => bcrypt('laravel'),
-            'profession_id' => 1,
+            'profession_id' => $professionId,
             'created_at' => $now,
             'updated_at' => $now
         ]);
