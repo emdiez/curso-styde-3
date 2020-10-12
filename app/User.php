ccdd08c728741ddbf901969d9b370c298e0145a3
Salomon Machuca Ubuntu 18.04 <salomonmachucaduran@gmail.com>
2020-10-12 16:46:40 -0500

    Leccion 15. Usando Eloquente de manera interactiva con Tinker. 	modified:   app/User.php

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'profession_id',
     ];
 
     /**
@@ -26,4 +26,14 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function isAdmin()
+    {
+        return (strcmp($this->email, 'em_di-es@hotmail.com') === 0);
+    }
+
+    public static function findByEmail($email)
+    {
+        return static::where(compact('email'))->first();
+    }
 }
